e1b8ecc2861ebabb332cebbe7c81e72115840670
Andrew Minkin <minkin.andrew@gmail.com>
2022-03-02 20:43:46 +0600

    fixed build

--- api/api.go
+++ api/api.go
@@ -7,7 +7,7 @@ import (
 
 type (
 	Server struct {
-		r      gin.Router
+		r      *gin.Engine
 		config *config.ShortsConfig
 	}
 )
@@ -23,6 +23,7 @@ func (s *Server) initRoutes() {
 	s.r.GET("/:url", s.showURL)
 	// The only kratos thing would be here
 	userAPI := s.r.Group("/api/")
+	userAPI.POST("/url", s.shortifyURL)
 	_ = userAPI
 
 	// TODO: Implement RBAC here
@@ -30,7 +31,7 @@ func (s *Server) initRoutes() {
 	_ = analyticsAPI
 
 }
-
+func (s *Server) shortifyURL(c *gin.Context) {}
 func (s *Server) Start() error {
 	return s.r.Run(s.config.BindAddr)
 }

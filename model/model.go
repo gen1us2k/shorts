37fb9b2b5ca4e875591acb2a4af17c2ff753f10e
Andrew Minkin <minkin.andrew@gmail.com>
2022-03-03 12:49:35 +0600

    Added oathkeeper, keto, kratos and designed models

--- model/model.go
+++ model/model.go
@@ -1,8 +1,24 @@
 package model
 
+import "time"
+
 type (
-	URL        struct{}
-	Referer    struct{}
-	User       struct{}
+	URL struct {
+		OriginalURL    string
+		CreationDate   time.Time
+		ExpirationDate time.Time
+		UserID         int64
+		User           User
+	}
+	Referer struct {
+		URL string
+	}
+	User struct {
+		FirstName    string
+		LastName     string
+		CreationDate string
+		LastLogin    string
+		Email        string
+	}
 	Statistics struct{}
 )

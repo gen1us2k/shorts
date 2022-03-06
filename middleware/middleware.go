7cff546c183b353bb90823a5989e420226c11870
Andrew Minkin <minkin.andrew@gmail.com>
2022-03-06 21:56:58 +0600

    Middleware design

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,13 @@
 package middleware
+
+import (
+	"github.com/gen1us2k/shorts/config"
+	"github.com/gin-gonic/gin"
+)
+
+type KratosMiddleware struct {
+	config *config.ShortsConfig
+}
+
+func (k *KratosMiddleware) User(c *gin.Context) {
+}

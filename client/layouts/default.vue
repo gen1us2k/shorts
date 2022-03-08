539e577ba33672b308a9f51938bec0d54048483a
Andrew Minkin <minkin.andrew@gmail.com>
2022-03-09 01:16:59 +0600

    Added AppHeader component

---
+++ client/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <AppHeader />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import AppHeader from '../components/AppHeader'
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>

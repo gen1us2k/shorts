20dc57e7eb4b824460fd7d429130e05533101f2e
Andrew Minkin <minkin.andrew@gmail.com>
2022-03-15 19:05:38 +0600

    Doc strings for database

--- database/database.go
+++ database/database.go
@@ -6,14 +6,19 @@ import (
 )
 
 type (
-	// WriteDatabase interface represents interface for
+	// WriteDatabase represents interface for
 	// a database layer with write permissions. For Analytics you need to implement
 	// Analytics interface
 	WriteDatabase interface {
+		// ShortifyURL makes a short version of URL.
 		ShortifyURL(model.URL) (model.URL, error)
+		// ListURLs returns all created urls by user
 		ListURLs(string) ([]model.URL, error)
+		// GetURLByHash returns an URL created by given hash
 		GetURLByHash(string) (model.URL, error)
+		// StoreView saves an URL view to the database
 		StoreView(model.Referer) error
+		// DeleteURL deletes URL
 		DeleteURL(model.URL) error
 	}
 	// Analytics interface required for analytics
@@ -23,6 +28,7 @@ type (
 	}
 )
 
+// CreateStorage creates a storage by given configuration
 func CreateStorage(c *config.ShortsConfig) (WriteDatabase, error) {
 	if c.DatabaseProvider == config.ProviderPostgres {
 		return NewPostgres(c)

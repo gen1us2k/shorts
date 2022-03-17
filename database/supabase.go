ec4899b2258f691043ad76bf90978d4cc700ad95
Andrew Minkin <minkin.andrew@gmail.com>
2022-03-17 19:48:06 +0600

    added docstrings

--- database/supabase.go
+++ database/supabase.go
@@ -11,12 +11,15 @@ import (
 	"github.com/supabase/postgrest-go"
 )
 
+// Supabase struct is an implementation
+// of database using Supabase Database service
 type Supabase struct {
 	WriteDatabase
 	config *config.ShortsConfig
 	conn   *postgrest.Client
 }
 
+// NewSupabase creates a new instance by given configuration
 func NewSupabase(c *config.ShortsConfig) (*Supabase, error) {
 	conn := postgrest.NewClient(c.SupabaseURL, "", map[string]string{
 		"apikey":        c.SupabaseKey,
@@ -31,6 +34,7 @@ func NewSupabase(c *config.ShortsConfig) (*Supabase, error) {
 	}, nil
 }
 
+// ShortifyURL generates short hash code of url and stores it in the database
 func (s *Supabase) ShortifyURL(url model.URL) (model.URL, error) {
 	var urls []model.URL
 	url.Hash = generateHash(s.config.URLLength)
@@ -39,12 +43,16 @@ func (s *Supabase) ShortifyURL(url model.URL) (model.URL, error) {
 	_, err := q.ExecuteTo(&urls)
 	return urls[0], err
 }
+
+// ListURLs returns all urls created by user
 func (s *Supabase) ListURLs(ownerID string) ([]model.URL, error) {
 	var urls []model.URL
 	q := s.conn.From("url").Select("*", "10", false).Match(map[string]string{"owner_id": ownerID}) // FIXME: Make this configurable
 	_, err := q.ExecuteTo(&urls)
 	return urls, err
 }
+
+// GetURLByHash returns URL from the database by given hash
 func (s *Supabase) GetURLByHash(hash string) (model.URL, error) {
 	var urls []model.URL
 	q := s.conn.From("url").Select("*", "10", false).Match(map[string]string{"hash": hash})
@@ -54,11 +62,15 @@ func (s *Supabase) GetURLByHash(hash string) (model.URL, error) {
 	}
 	return urls[0], err
 }
+
+// StoreView stores view
 func (s *Supabase) StoreView(ref model.Referer) error {
 	q := s.conn.From("url_view").Insert(ref, false, "do-nothing", "", "")
 	_, _, err := q.Execute()
 	return err
 }
+
+// DeleteURL deletes URL
 func (s *Supabase) DeleteURL(url model.URL) error {
 	q := s.conn.From("url").Delete("", "").Match(map[string]string{"id": strconv.FormatInt(url.ID, 10)})
 	_, _, err := q.Execute()
